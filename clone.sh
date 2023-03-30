c578b0f93b3c51b883d28ed672d61adff622be66
Asif <49162687+nhAsif@users.noreply.github.com>
2023-03-30 10:06:35 +0600

    Update clone.sh

--- clone.sh
+++ clone.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 # Clone Kernel
-git clone --depth=1 https://github.com/Jasmine-Trees/Realking_kernel_sm8250.git -b alioth kernel
+git clone --depth=1 https://github.com/Poco-F3-Trees/kernel_xiaomi_sm8250.git -b staging kernel
 cp -r kbuild.sh kernel/
