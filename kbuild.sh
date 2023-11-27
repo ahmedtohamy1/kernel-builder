9bc8f0dc6adbee8b1c89e8343645a85dd05e4234
Asif <49162687+nhAsif@users.noreply.github.com>
2023-11-27 12:37:26 +0600

    Update kbuild.sh

--- kbuild.sh
+++ kbuild.sh
@@ -126,7 +126,7 @@ function cloneTC() {
 }
         # Clone AnyKernel
       if [ ! -d "AnyKernel3" ]; then
-    git clone --depth=1 https://github.com/Poco-F3-Trees/AnyKernel3 -b alioth AnyKernel3
+    git clone --depth=1 https://github.com/levi3609/AnyKernel3 -b alioth AnyKernel3
       fi
 	
 # Export Variables
