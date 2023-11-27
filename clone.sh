05f6b288a3931bd12865bd32bf496ce366e14e8a
Asif <49162687+nhAsif@users.noreply.github.com>
2023-11-27 15:43:18 +0600

    Update clone.sh

--- clone.sh
+++ clone.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 # Clone Kernel
-git clone https://github.com/levi3609/kernel_xiaomi_sm8250 -b staging kernel
+git clone --depth=1 https://github.com/levi3609/kernel_xiaomi_sm8250 -b staging kernel
 cp -r kbuild.sh kernel/
