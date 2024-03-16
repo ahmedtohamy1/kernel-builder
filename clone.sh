df20a52e15c45c2ec30b47e38ec9a8b6b35bbf3f
Asif <49162687+nhAsif@users.noreply.github.com>
2024-03-17 05:02:03 +0600

    Update clone.sh

--- clone.sh
+++ clone.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 # Clone Kernel
-git clone https://github.com/levi3609/kernel_xiaomi_sm8250-redline --depth 1 -b aosp-14 kernel
+git clone https://github.com/sm8250-stuffs/kernel_xiaomi_sm8250 --depth 1 -b ksu kernel
 cp -r kbuild.sh kernel/
